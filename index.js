3d51ad2b7ba21d2defd3e3608b534dac11ed0d6e
Michelle <clairsin@gmail.com>
2022-04-25 16:25:11 -0400

    commit tree 8e5d1dc95f1ba4e262b7ac906320d3da6f364f2e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Sinclair Shang'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+console.log(currentUser.slice(0,1))
